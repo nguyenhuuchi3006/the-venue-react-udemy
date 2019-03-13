f4f1d6fcd030291b79f76814746e6e5eacb4601b
Chi Barca <nguyenhuuchi3006@gmail.com>
2019-03-13 22:48:56 +0700

    Boilerplate a react project and code AppBar (giong Navbar)

---
+++ src/components/header_footer/Header.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+class Header extends Component {
+    render() {
+        return (
+            <AppBar
+                position="static" 
+                style={{
+                    backgroundColor: '#2f2f2f',
+                    boxShadow: 'none',
+                    padding: '10px 0px'
+                }}
+                
+            >
+                <Toolbar>
+                    <div className="header_logo">
+                        <div className="font_righteous header_logo_venue">The Venue</div>
+                        <div className="header_logo_title">Musical Event</div>
+                    </div>
+
+
+
+                </Toolbar>
+            </AppBar> 
+        );
+    }
+}
+
+export default Header;
