525c503338868a6b0458023f043aef0c5821b464
Chi Barca <nguyenhuuchi3006@gmail.com>
2019-03-14 20:27:09 +0700

    complete countdown

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carrousel from './Carrousel';
+import CountDown from './CountDown';
 
 const Featured = () => {
     return (
@@ -12,6 +13,9 @@ const Featured = () => {
                     Ariana Grande
                 </div>
             </div>
+
+            <CountDown />
+
         </div>
     );
 };
